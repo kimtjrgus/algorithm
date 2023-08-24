29912bba1e9e5e9e840bc78c9565265a3d52b1f6
kimtjrgus <sh95921@gmail.com>
2023-08-24 19:44:22 +0900

    Feat : 정수삼각형

---
+++ DP/level3/IntTriangle.java
@@ -0,0 +1,40 @@
+package DP.level3;
+
+import java.util.Arrays;
+
+public class IntTriangle { // DP 동적계획법 : 정수삼각형
+    public static void main(String[] args) {
+        int[][] triangle = new int[][]{{7}, {3, 8}, {8, 1, 0}, {2, 7, 4, 4}, {4, 5, 2, 6, 5}};
+        // 입력 예시
+
+        System.out.println(solution(triangle));
+        // 출력 30
+    }
+
+    private static int solution(int[][] triangle) {
+        int[][] dp = new int[triangle.length][triangle.length];
+        dp[0][0] = triangle[0][0];
+
+        for (int i = 1; i < triangle.length; i++) {
+            // 맨 왼쪽
+            dp[i][0] = dp[i - 1][0] + triangle[i][0];
+
+            // 중간
+            for (int j = 1; j <= i; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - 1]) + triangle[i][j];
+            }
+
+            // 맨 오른쪽
+            dp[i][i] = dp[i - 1][i - 1] + triangle[i][i];
+        }
+        System.out.println(Arrays.deepToString(dp));
+
+        int answer = 0;
+
+        for (int i = 0; i < triangle.length; i++) {
+            answer = Math.max(answer, dp[triangle.length - 1][i]);
+        }
+
+        return answer;
+    }
+}

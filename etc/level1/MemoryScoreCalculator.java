5c8c301434e1f71bfa3b5646512ec3b647beec63
kimtjrgus <sh95921@gmail.com>
2023-08-24 19:46:52 +0900

    Feat : 추억점수

--- etc/level1/MemoryScoreCalculator.java
+++ etc/level1/MemoryScoreCalculator.java
@@ -5,6 +5,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MemoryScoreCalculator {
+    public static void main(String[] args) {
+        String[] name = {"may", "kein", "kain", "radi"};
+        int[] yearning = {5, 10, 1, 3};
+        String[][] photo = {{"may", "kein", "kain", "radi"},
+                {"may", "kein", "brin", "deny"},
+                {"kon", "kain", "may", "coni"}};
+
+        int[] answer = solution(name, yearning, photo);
+        System.out.println(
+                "answer: "+ Arrays.toString(answer));
+    }
+
     public static int[] solution(String[] name, int[] yearning, String[][] photo) {
         int[] answer = new int[photo.length];
 
@@ -26,18 +38,6 @@ public class MemoryScoreCalculator {
         }
 
         return answer;
-    }
-
-    public static void main(String[] args) {
-        String[] name = {"may", "kein", "kain", "radi"};
-        int[] yearning = {5, 10, 1, 3};
-        String[][] photo = {{"may", "kein", "kain", "radi"},
-                {"may", "kein", "brin", "deny"},
-                {"kon", "kain", "may", "coni"}};
-
-        int[] answer = solution(name, yearning, photo);
-        System.out.println(
-                "answer: "+ Arrays.toString(answer));
     }
 }
 

f7c66dee55e464c432d767a17f9869b7778cea93
kimtjrgus <sh95921@gmail.com>
2023-08-28 14:57:58 +0900

    feat : 단어 변환

--- BFS_DFS/level3/WordConversion.java
+++ BFS_DFS/level3/WordConversion.java
@@ -1,16 +1,64 @@
 package BFS_DFS.level3;
 
-public class WordConversion {// TODO: 2023-08-25
+import java.util.*;
+
+public class WordConversion {
     public static void main(String[] args) {
-        System.out.println();
-        System.out.println();
+        String[] words1 = new String[]{"hot", "dot", "dog", "lot", "log", "cog"};
+        System.out.println(solution("hit", "cog", words1)); // 4
+
+        String[] words2 = new String[]{"hot", "dot", "dog", "lot", "log"};
+        System.out.println(solution("hit", "cog", words2)); // 0
     }
 
-    public int solution(String begin, String target, String[] words) {
+    // DFS, BFS 알고리즘 사용 -> 검색경로가 크지 않은 최소경로는 BFS가 효과적
+    public static int solution(String begin, String target, String[] words) {
         int answer = 0;
-        return answer;
 
+        Queue<String> queue = new LinkedList<>(); // 변환 가능한 단어들을 저장할 queue
+        Set<String> set = new HashSet<>(Arrays.asList(words)); // 단어들의 집합 set
 
+        if(!set.contains(target)){
+            return 0; // 변환할 수 없는 경우
+        }
+
+        queue.offer(begin); // queue에 begin 단어 추가
+        set.remove(begin); // set에서 begin 단어 제거
+
+        while (!queue.isEmpty()){
+
+            for (int i=0; i<queue.size(); i++){
+                // queue에서 단어를 하나씩 꺼내어 current (현재단어)에 저장
+                String current = queue.poll();
+
+                if (current.equals(target)){
+                    return answer; // 타겟 단어에 도달
+                }
+
+                // HashSet 타입은 for 루프를 사용할 수 없으므로 to.Array()를 사용해 배열로 변환
+                for (String word : set.toArray(new String[set.size()])){
+                    // 변환 가능하면 queue에 해당 단어를 추가하고 set에서 제거
+                    if(canConvert(current,word)){
+                        queue.offer(word);
+                        set.remove(word);
+                    }
+                }
+            }
+
+            answer++;
+        }
+
+        return 0; // 변환할 수 없는 경우
+    }
+
+    private static boolean canConvert(String word1, String word2){
+        int diffCnt = 0;
+        for (int i = 0; i<word1.length(); i++){
+            if (word1.charAt(i) != word2.charAt(i)){
+                diffCnt++;
+            }
+        }
+        return diffCnt ==1;
     }
 }
 

b56b47ab04991b59d0ba8dd35049110986c97fec
김석현 <119948479+kimtjrgus@users.noreply.github.com>
2023-09-13 14:48:24 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.05 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -6,7 +6,7 @@ class Solution {
         // 제곱근이 정수인지 판별
         if(sqrt == (int)sqrt){
             // 정수라면 sqrt +1 후 제곱하여 answer 구하기
-            return answer = (long)((sqrt+1)* (sqrt+1));
+            return answer = (long)(Math.pow(sqrt + 1, 2));
         }
         else return answer = -1;
     }

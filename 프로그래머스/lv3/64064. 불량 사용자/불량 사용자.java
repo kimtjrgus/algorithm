7ffeee07292531ad36575cad27ad9f0a55d367da
김석현 <119948479+kimtjrgus@users.noreply.github.com>
2023-09-18 14:46:16 +0900

    [level 3] Title: 불량 사용자, Time: 0.55 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.java
@@ -1,43 +1,39 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
-class Solution {
+public class Solution {
     public int solution(String[] user_id, String[] banned_id) {
-        Set<Set<String>> combinations = new HashSet<>();
-        boolean[] visited = new boolean[user_id.length];
-        generateCombinations(user_id, banned_id, combinations, new HashSet<>(), visited, 0);
-        return combinations.size();
+        Set<Set<String>> validComb = new HashSet<>();
+        findValidCombinations(user_id, banned_id, 0, new HashSet<>(), validComb);
+        return validComb.size();
     }
-    
-    private void generateCombinations(String[] user_id, String[] banned_id, Set<Set<String>> combinations, Set<String> currentCombination, boolean[] visited, int index) {
+
+    public void findValidCombinations(String[] user_id, String[] banned_id, int index, Set<String> curComb, Set<Set<String>> validComb) {
         if (index == banned_id.length) {
-            // 현재 조합이 불량 사용자 아이디 수와 일치할 때
-            if (currentCombination.size() == banned_id.length) {
-                combinations.add(new HashSet<>(currentCombination));
-            }
+            validComb.add(new HashSet<>(curComb));
             return;
         }
-        
-        for (int i = 0; i < user_id.length; i++) {
-            if (!visited[i] && isMatch(user_id[i], banned_id[index])) {
-                visited[i] = true;
-                currentCombination.add(user_id[i]);
-                generateCombinations(user_id, banned_id, combinations, currentCombination, visited, index + 1);
-                visited[i] = false;
-                currentCombination.remove(user_id[i]);
+
+        String currentBannedId = banned_id[index];
+        for (String userId : user_id) {
+            if (!curComb.contains(userId) && isValid(userId, currentBannedId)) {
+                curComb.add(userId);
+                findValidCombinations(user_id, banned_id, index + 1, curComb, validComb);
+                curComb.remove(userId);
             }
         }
     }
-    
-    private boolean isMatch(String user, String banned) {
+
+    public boolean isValid(String user, String banned) {
         if (user.length() != banned.length()) {
             return false;
         }
+
         for (int i = 0; i < user.length(); i++) {
             if (banned.charAt(i) != '*' && banned.charAt(i) != user.charAt(i)) {
                 return false;
             }
         }
+
         return true;
     }
 }

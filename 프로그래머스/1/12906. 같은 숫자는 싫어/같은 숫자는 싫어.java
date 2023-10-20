e427858e3489f5f820b5ac355ee8d74058ee54b0
김석현 <119948479+kimtjrgus@users.noreply.github.com>
2023-10-20 16:20:05 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 25.66 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,14 +2,12 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
-        // 인덱스 두개를 비교해서 반복될 경우 제거할건데 어떤식으로 제거할까
-        // 새 배열에 추가하는 식이 좋을거같네
-        // arr 배열을 비교하고 answer배열에 추가하는 식. let's go
-        // 1. 해답이 될 answer 배열 선언
+        // 1. 배열은 고정크기, 리스트는 가변크기
+        // 리스트로 선언, 차후 배열로 변환
         ArrayList<Integer> answerList = new ArrayList<>();
         answerList.add(arr[0]);
         
-        // 2. arr 배열을 반복하며 answer배열에 추가
+        // 2. arr 배열을 반복하며 answerList 추가
         for(int i=1; i<arr.length; i++){
             if(arr[i] != arr[i-1]){ // 숫자가 반복되지 않을 경우
                 answerList.add(arr[i]);

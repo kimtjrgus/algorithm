d516464cedde23cbf7dcdc18716243ec92e9a8b4
김석현 <119948479+kimtjrgus@users.noreply.github.com>
2023-10-20 21:53:04 +0900

    [level 1] Title: 예산, Time: 0.33 ms, Memory: 70.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        // 예산은 정해져있고, 최대한 많은 부서
+        // 신청한 금액 모두 지원을 해줘야함 -> 오름차순 
+        // 신청금액 배열 d, 예산 budget
+        int answer = 0; // 지원해준 부서의 수
+        int sum = 0; // 구매한 비용
+        
+        Arrays.sort(d); // 오름차순 정렬
+        for(int i=0; i<d.length; i++){
+            if(budget-d[i] >= 0){
+                budget -= d[i];
+                sum += d[i];
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+} 

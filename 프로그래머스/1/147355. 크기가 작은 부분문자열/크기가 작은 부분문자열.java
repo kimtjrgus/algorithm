bc4095c580bb0e912b521a24abab08c551f40fa7
김석현 <119948479+kimtjrgus@users.noreply.github.com>
2023-10-28 14:46:47 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.09 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -3,33 +3,20 @@ class Solution {
         // p.length() 길이파악
         // t의 부분문자열 tpart 추출 후, 
         // for (p >= tpart) count++
+        int pLength = p.length();
+        long pValue = Long.parseLong(p);
         int count = 0;
-        int lenP = p.length();
-        int lenT = t.length();
-
-        for (int i = 0; i <= lenT - lenP; i++) {
-            if (isSmallerOrEqual(t, i, p)) {
+        
+        for (int i = 0; i <= t.length() - pLength; i++) {
+            long tValue = Long.parseLong(t.substring(i, i + pLength));
+            
+            if (tValue <= pValue)
                 count++;
-            }
         }
-
+        
         return count;
     }
-
-// 두 숫자로만 이루어진 문자열을 비교하는 메서드
-    private boolean isSmallerOrEqual(String t, int startIndex, String p) {
-        for (int i = 0; i < p.length(); i++) {
-            if (t.charAt(startIndex + i) < p.charAt(i)) {
-                return true;  // p가 더 큰 경우가 아니면
-            } else if (t.charAt(startIndex + i) > p.charAt(i)) {
-                return false;  // p가 더 작은 경우
-            }
-        }
-        return true;  // p와 t 부분 문자열이 같은 경우
-    }
 }
-
-
 
 // 숫자로만 이루어진 문자열 t p 
 // t 에서 /p와 길이가 같은 부분문자열 중 p보다 작거나 같은 것의 가짓수

21e978cd450028fbdf2f5537967acc3dd61ecd96
김석현 <119948479+kimtjrgus@users.noreply.github.com>
2023-11-09 13:14:40 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 9.71 ms, Memory: 96.1 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -22,4 +22,20 @@ class Solution {
         
         return answer;
     }
+    /*
+    public int[] solution(String str) {
+        int[] result = new int[str.length()];
+
+        for(int i=0;i<str.length();i++){
+
+            String subStr = str.substring(0,i);
+            if(subStr.indexOf(str.charAt(i))==-1) {
+                result[i] = -1;
+            }else {
+                result[i] = i-subStr.lastIndexOf(str.charAt(i));
+            }
+        }
+        return result;
+    }
+    */
 }

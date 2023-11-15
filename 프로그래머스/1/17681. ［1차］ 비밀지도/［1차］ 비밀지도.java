09680830d885e8166ab9abb678b645987df8b568
김석현 <119948479+kimtjrgus@users.noreply.github.com>
2023-11-15 15:43:28 +0900

    [level 1] Title: [1차] 비밀지도, Time: 7.71 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -5,8 +5,6 @@ class Solution {
         for (int i = 0; i < n; i++) {
             // arr1, arr2 각 숫자를 2진법으로 변환하여 OR 연산을 수행
             int combined = arr1[i] | arr2[i];
-
-            // 2진법으로 변환한 숫자를 비밀지도 형식으로 변환
             String binaryString = Integer.toBinaryString(combined);
 
             // 길이가 n이 될 때까지 앞에 0을 채워줌
@@ -16,7 +14,6 @@ class Solution {
             binaryString = binaryString.replaceAll("1", "#");
             binaryString = binaryString.replaceAll("0", " ");
 
-            // 결과 배열에 저장
             answer[i] = binaryString;
         }
 

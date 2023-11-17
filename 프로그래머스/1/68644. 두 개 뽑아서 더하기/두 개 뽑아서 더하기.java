ae92d7779bc7650e3176d4c9400f416b05ec6311
김석현 <119948479+kimtjrgus@users.noreply.github.com>
2023-11-17 10:50:24 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 4.03 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        // 서로 다른 인덱스의 두수를 더해 만들 수 있는 모든 수
+        // 1. 인덱스0 부터 모든 수와 더한 후 리스트에 추가.
+        // 2. 중복제거, 반복 이중for문
+        List<Integer> sumList = new ArrayList<Integer>(); 
+        
+        for(int i=0; i<numbers.length; i++){
+            for(int j=i+1; j<numbers.length; j++){
+                int sum = numbers[i] + numbers[j];
+                sumList.add(sum);
+            }
+        }
+        List<Integer> answerList = sumList.stream()
+                                .distinct().collect(Collectors.toList());
+        
+        int[] answer = answerList.stream().mapToInt(i->i).toArray();
+        Arrays.sort(answer);
+        
+        return answer;
+    }
+}

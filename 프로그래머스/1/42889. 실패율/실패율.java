13c3f0a36ff591f27aed13c2104a7b7ae94c81b8
김석현 <119948479+kimtjrgus@users.noreply.github.com>
2023-12-01 15:46:35 +0900

    [level 1] Title: 실패율, Time: 0.05 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int N, int[] stages) {
-        // answer은 실패율이 높은 스테이지, 내림차순, 길이는 N
+        // answer은 실패율이 높은 스테이지, 실패율 높은 순, 길이는 N
         // 1. 길이 N의 정답배열 선언, 초기화
         int[] answer = new int[N]; 
         Arrays.fill(answer, 0);
@@ -24,7 +24,7 @@ class Solution {
         }
         
         
-        // 5. answer에 넣고 내림차순정렬
+        // 5. 실패율이 높은 순 정리
         for (int i = 0; i < N; i++) {
             double maxFailure = -1;
             int maxStage = 0;
@@ -44,7 +44,3 @@ class Solution {
         return answer;
     }
 }
-// 실패율 : 도달o 클리어x 유저수/ 도달한 유저수
-// N : 전체 스테이지수  
-// stages : 사용자가 멈춰있는 스테이지 번호가 담긴 배열 = 배열의 길이는 사용자수
-// stages의 번호는 현재 도전중인 스테이지 번호

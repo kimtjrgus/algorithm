9f7496813759e39c540196f56b9650db6d4790f4
김석현 <119948479+kimtjrgus@users.noreply.github.com>
2023-12-26 13:14:14 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 90.39 ms, Memory: 96.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,27 +1,28 @@
-import java.util.Arrays;
+import java.util.HashMap;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
         // comp는 part보다 길이 1 짧음
         // 동명이인이 있을 수 있음. 1명만 체크하라는 뜻인듯
         String answer = ""; // answer = 완주 못한 선수
-        // 1. 반복문은 케이스가 너무 많아서 비효율적
-        // 2. 해시 ?
         
-        // 1. 두 배열을 정렬한다
-        Arrays.sort(participant);
-        Arrays.sort(completion);
-
-        // 2. 두 배열이 다를 때까지 찾는다
-        int i = 0;
-        for(i=0;i<completion.length;i++)
-            if(!participant[i].equals(completion[i]))
-                break;
-
-
-        // 3. 여기까지 왔다는 것은 마지막 주자가 완주하지 못했다는 의미이다.
-        answer = participant[i];
+        // 1. 해시맵에 part 추가 key는 선수, value는 선수카운트
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String p : participant){
+            map.put(p, map.getOrDefault(p, 0) + 1);
+        }
+        // 2. comp를 반복, 완주했으면 카운트 마이너스
+        for(String p : completion){
+            map.put(p, map.get(p) - 1);
+        }
         
+        // 3. value가 0이 아닌 선수 1명만 남으니 return
+        for(String key : map.keySet()){
+            if(map.get(key) != 0){
+                answer = key;
+            }
+        }
         return answer;
     }
 }
+    

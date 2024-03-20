0df42fa7796eb8bd022572143abb1b19baac6bd7
Kim 김석현 <119948479+kimtjrgus@users.noreply.github.com>
2024-03-20 13:53:51 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.02 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -0,0 +1,78 @@
+import java.util.*; 
+/*
+@param lottos   : 내가 구매한 로또번호
+@param win_nums : 당첨번호
+@return : 민우가 당첨 가능한 [최고순위, 최저순위] 
+
+알아볼수 없는 번호는 -> 0 표기 (당첨번호거나, 아니거나)
+같은 숫자는 없고, 정렬안되어있음
+*/
+class Solution {       
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int winCount = 0; // 당첨된 수
+        int zeroCount = 0; // 0의 갯수 
+        int[] answer = new int[2];
+
+        // 1. lottos 에서 0을 제외한 나머지가 당첨되는지 -> 몇개매칭
+        for(int lotto : lottos){
+            // 2. 0의 갯수 카운트
+            if(lotto == 0){
+                zeroCount++; 
+                continue;
+            }
+            else{
+                for(int i= 0; i<win_nums.length; i++){
+                    if(lotto == win_nums[i]){
+                        winCount++;
+                    }
+                }
+            }
+            
+        }
+        // 1등:6/ 2등:5/ 3등:4/ 4등-3/ 5-2/ 6-1
+        switch(winCount + zeroCount) {
+            case 6:
+                answer[0] = 1;
+                break;
+            case 5:
+                answer[0] = 2;
+                break;
+            case 4:
+                answer[0] = 3;
+                break;
+            case 3:
+                answer[0] = 4;
+                break;
+            case 2:
+                answer[0] = 5;
+                break;
+            default:
+                answer[0] = 6;
+                break;
+        }
+        
+        // 최저 순위 계산
+        switch(winCount) {
+            case 6:
+                answer[1] = 1;
+                break;
+            case 5:
+                answer[1] = 2;
+                break;
+            case 4:
+                answer[1] = 3;
+                break;
+            case 3:
+                answer[1] = 4;
+                break;
+            case 2:
+                answer[1] = 5;
+                break;
+            default:
+                answer[1] = 6;
+                break;
+        }
+        
+        return answer;
+    }
+}

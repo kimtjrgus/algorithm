2b9401956e5e0460fb5ea8cf4c291d55bdce2fd9
Kim 김석현 <119948479+kimtjrgus@users.noreply.github.com>
2024-03-27 11:16:40 +0900

    [level 2] Title: 올바른 괄호, Time: 17.86 ms, Memory: 53 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                if (stack.isEmpty() || stack.pop() != '(') {
+                    return false; // 스택이 비어있거나 짝이 맞지 않는 경우
+                }
+            }
+        }
+        System.out.println(stack);
+
+        return stack.isEmpty();
+    }
+}

f45d8b7dfa926ddae25959e82a6e5ca6702c0937
Kim 김석현 <119948479+kimtjrgus@users.noreply.github.com>
2024-06-27 13:59:32 +0900

    [level 2] Title: 롤케이크 자르기, Time: 196.51 ms, Memory: 169 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -3,26 +3,29 @@ import java.util.*;
 class Solution {
     public int solution(int[] topping) {
         int result = 0;
-        Map<Integer, Integer> chulsu = new HashMap<>();
-        Map<Integer, Integer> bro = new HashMap<>();
+        Set<Integer> chulsuSet = new HashSet<>();
+        Map<Integer, Integer> broMap = new HashMap<>();
         
         // 동생의 해쉬맵에 먼저 가득 채워넣고, 철수의 Map은 비워놓음.
-        for(int t : topping){
-            bro.put(t, bro.getOrDefault(t, 0) + 1);
+        for (int t : topping) {
+            broMap.put(t, broMap.getOrDefault(t, 0) + 1);
         }
         
-        // 배열을 순회하며 철수의 Map을 채워주고
+        // 배열을 순회하며 철수의 Set을 채워주고
         for (int t : topping) {
-            chulsu.put(t, chulsu.getOrDefault(t, 0) + 1);
+            chulsuSet.add(t);
             
-            if (bro.get(t) - 1 == 0)
-                bro.remove(t);
-            else
-                bro.put(t, bro.get(t) - 1);  
+            // 동생의 해쉬맵에서 토핑 하나를 줄임
+            if (broMap.get(t) - 1 == 0) {
+                broMap.remove(t);
+            } else {
+                broMap.put(t, broMap.get(t) - 1);
+            }
             
-            // 가짓수를 세어 Map.size가 같다면 result++
-            if (chulsu.size() == bro.size())
+            // 가짓수를 세어 chulsuSet.size()가 broMap.size()와 같다면 result++
+            if (chulsuSet.size() == broMap.size()) {
                 result++;
+            }
         }
         
         return result;
@@ -31,4 +34,4 @@ class Solution {
 
 // 잘린 조각크기와 토핑개수는 상관x
 // 각 조각에 동일한 가짓수의 토핑만 올라가면 됨
-// 토핑종류 상관x, 가짓수만 같으면 됨
+// 토핑종류 상관x, 가짓수만 같으면 됨 -> HashSet 활용

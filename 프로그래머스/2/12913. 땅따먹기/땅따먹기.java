c7c1b4c7a4ba1fe022d5c18bfcc6479ca4b0c1aa
Kim 김석현 <119948479+kimtjrgus@users.noreply.github.com>
2024-07-03 16:18:57 +0900

    [level 2] Title: 땅따먹기, Time: 19.61 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -2,16 +2,16 @@ class Solution {
     int solution(int[][] land) {
         int rows = land.length;
 
-        // 각 행을 순회하며 점수를 갱신합니다.
+        // 각 행을 순회
         for (int i = 1; i < rows; i++) {
-            // 각 열에 대해 가능한 최대 점수를 갱신합니다.
+            // 각 열에 대해 가능한 최대 점수를 갱신
             land[i][0] += Math.max(Math.max(land[i - 1][1], land[i - 1][2]), land[i - 1][3]);
             land[i][1] += Math.max(Math.max(land[i - 1][0], land[i - 1][2]), land[i - 1][3]);
             land[i][2] += Math.max(Math.max(land[i - 1][0], land[i - 1][1]), land[i - 1][3]);
             land[i][3] += Math.max(Math.max(land[i - 1][0], land[i - 1][1]), land[i - 1][2]);
         }
 
-        // 마지막 행에서 최대값을 찾아 반환합니다.
+        // 마지막 행에서 최대값을 찾아 반환
         return Math.max(Math.max(land[rows - 1][0], land[rows - 1][1]),
                         Math.max(land[rows - 1][2], land[rows - 1][3]));
     }

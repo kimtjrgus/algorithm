63a9810d66390fd8a6b4ba2f27970fbab35f0fa2
Kim 김석현 <119948479+kimtjrgus@users.noreply.github.com>
2024-08-10 15:52:30 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 88.87 ms, Memory: 138 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -3,20 +3,13 @@ import java.util.*;
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
         // 큐 생성 후 초기 셋팅
-        Queue<Integer> que1 = new LinkedList<Integer>();
-        Queue<Integer> que2 = new LinkedList<Integer>();
-        long sum1 = 0, sum2 = 0;
+        Queue<Integer> que1 = new LinkedList<>();
+        Queue<Integer> que2 = new LinkedList<>();
         
         // 큐에 원소 추가 및 합 계산
-        for (int i = 0; i < queue1.length; i++) {
-            que1.add(queue1[i]);
-            sum1 += queue1[i];
-        }
+        long sum1 = initializeQueueAndGetSum(queue1, que1);
+        long sum2 = initializeQueueAndGetSum(queue2, que2);
         
-        for (int i = 0; i < queue2.length; i++) {
-            que2.add(queue2[i]);
-            sum2 += queue2[i];
-        }
         
         // 두 큐의 합이 같아질 때까지 원소 이동 // 가능한 최대 연산 횟수
         int maxAct = (queue1.length + queue2.length) * 2; 
@@ -28,18 +21,27 @@ class Solution {
                 que2.add(element);
                 sum1 -= element;
                 sum2 += element;
-            } 
-            else {
+            } else {
                 int element = que2.poll();
                 que1.add(element);
                 sum2 -= element;
                 sum1 += element;
             }
-            
             answer++;
         }
+        
         // 연산 종료 후 두 큐의 합이 같으면 연산 횟수를 반환, 아니면 -1 반환
         return sum1 == sum2 ? answer : -1;
+    }
+    
+    // 큐 초기화 및 합 계산 메서드
+    private long initializeQueueAndGetSum(int[] array, Queue<Integer> queue) {
+        long sum = 0;
+        for (int num : array) {
+            queue.add(num);
+            sum += num;
+        }
+        return sum;
     }
 }
 
@@ -49,4 +51,8 @@ class Solution {
 이때 최소 작업 횟수
 
 불가능할 시 return -1
+
+--> 최적화
+# 중복코드 제거 : 두 큐의 원소 합을 계산하는 로직이 반복 -> 추상화
+
 */

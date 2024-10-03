986046ac106a10bd1f450a24ffb96c481c66c8f2
Kim 김석현 <119948479+kimtjrgus@users.noreply.github.com>
2024-10-03 10:36:30 +0900

    [level 2] Title: 테이블 해시 함수, Time: 13.40 ms, Memory: 142 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -6,15 +6,16 @@ class Solution {
         // 1. 주어진 col 번째 컬럼을 기준으로 오름차순 정렬
         //    같은 값일 경우 기본키인 첫 번째 컬럼을 기준으로 내림차순 정렬
         Arrays.sort(data, (a, b) -> {
-            if (a[col - 1] == b[col - 1]) {
-                return b[0] - a[0]; // 기본키 기준 내림차순
-            } else {
-                return a[col - 1] - b[col - 1]; // col 번째 컬럼 기준 오름차순
-            }
+            // 기본키 기준 내림차순    
+            if (a[col - 1] == b[col - 1]) return b[0] - a[0];
+            
+            // col 번째 컬럼 기준 오름차순
+            else return a[col - 1] - b[col - 1]; 
         });
 
         // 2. row_begin 부터 row_end 까지의 행에 대해 S_i 계산 및 XOR 누적
-        int hashValue = 0;
+        int hashValue = 0; // 해시값 선언
+        
         for (int i = row_begin - 1; i < row_end; i++) { // 인덱스는 0부터 시작하므로 -1
             int sum = 0;
             for (int value : data[i]) {
@@ -23,8 +24,9 @@ class Solution {
             }
             hashValue ^= sum; // XOR 연산
         }
-
-        return hashValue; // 최종 해시값 반환
+        
+        // return 최종 해시값 
+        return hashValue; 
     }
 }
 // 정렬된 데이터에서 S_i를 i 번째 행의 튜플에 대해 각 컬럼의 값을 i 로 나눈 

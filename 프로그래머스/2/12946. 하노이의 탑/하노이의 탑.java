9eafc13dd6f1067b1ecdf7fd918b35509dab34d1
Kim 김석현 <119948479+kimtjrgus@users.noreply.github.com>
2024-11-16 19:17:17 +0900

    [level 2] Title: 하노이의 탑, Time: 2.80 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -1,7 +1,8 @@
-class Solution {
-    int index = 0;
+class Solution { // 하노이의 탑 재귀용법
+    int index = 0; // 이동 인덱스
     
     public int[][] solution(int n) {
+        // 이동 횟수 2^n -1
         int count = (int)Math.pow(2, n) - 1;
         int[][] answer = new int[count][2];
         
@@ -10,12 +11,14 @@ class Solution {
         return answer;
     }
     
-    public int[][] hanoi(int n, int from, int by, int to, 
+    // from은 시작, by는 거쳐가는 기둥, to는 도착지 기둥
+    public int[][] hanoi(int n, 
+                         int from, int by, int to, 
                          int[][] answer) {
-        
         if(n==1) {
             answer[index++] = new int[] { from, to };
-        } else {
+        } 
+        else {
             hanoi(n-1, from, to, by, answer); // 1->3->2
             answer[index++] = new int[] { from, to };
             hanoi(n-1, by, from, to, answer); // 2->1->3
@@ -23,5 +26,4 @@ class Solution {
         
         return answer;
     }
-    
 }

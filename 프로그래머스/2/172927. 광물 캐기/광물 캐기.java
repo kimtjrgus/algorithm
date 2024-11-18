63dbdb156b8fbe0fc62e0f6c6b90212419217bc3
Kim 김석현 <119948479+kimtjrgus@users.noreply.github.com>
2024-11-19 01:46:03 +0900

    [level 2] Title: 광물 캐기, Time: 0.88 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -6,13 +6,12 @@ class Solution {
         // 예외, 곡괭이 수가 광물 수보다 부족할 경우
         int maxMinerals = 5 * (picks[0] + picks[1] + picks[2]); 
         minerals = Arrays.copyOfRange(minerals, 0, Math.min(maxMinerals, minerals.length));
-
         
         // 1. 광물 구간별 [다이아, 철, 돌] 개수 기록
         List<int[]> mineralCounts = new ArrayList<>();
-        for (int i = 0; i < minerals.length; i += 5) {
+        for (int i=0; i < minerals.length; i +=5) {
             int[] count = new int[3]; // [다이아, 철, 돌] 개수
-            for (int j = i; j < Math.min(i + 5, minerals.length); j++) {
+            for (int j=i; j < Math.min(i+5, minerals.length); j++){
                 if (minerals[j].equals("diamond")) count[0]++;
                 else if (minerals[j].equals("iron")) count[1]++;
                 else count[2]++;
@@ -20,30 +19,32 @@ class Solution {
             mineralCounts.add(count);
         }
 
-        // 2. 구간별 피로도 계산 (최악의 경우 돌 곡괭이로 캤을 때)
+        // 2. 구간별 피로도 계산 (최악의 경우,돌괭이로 캤을 때)
         mineralCounts.sort((a, b) -> {
             int worstA = a[0] * 25 + a[1] * 5 + a[2];
             int worstB = b[0] * 25 + b[1] * 5 + b[2];
             return Integer.compare(worstB, worstA); // 내림차순 정렬
         });
 
-        // 3. 곡괭이 사용하며 최소 피로도 계산
+        // 3. 최소 피로도 계산
         int answer = 0;
         for (int i = 0; i < mineralCounts.size(); i++) {
             int[] count = mineralCounts.get(i);
 
             if (picks[0] > 0) { // 다이아 곡괭이 사용
-                answer += count[0] + count[1] + count[2]; // 피로도 1씩
+                answer += count[0] + count[1] + count[2]; 
                 picks[0]--;
-            } else if (picks[1] > 0) { // 철 곡괭이 사용
-                answer += count[0] * 5 + count[1] + count[2]; // 다이아 5, 나머지 1
+            } 
+            else if (picks[1] > 0){ // 철괭이 사용
+                answer += count[0]*5 + count[1] + count[2];
                 picks[1]--;
-            } else if (picks[2] > 0) { // 돌 곡괭이 사용
-                answer += count[0] * 25 + count[1] * 5 + count[2]; // 다이아 25, 철 5, 돌 1
+            } 
+            else if (picks[2] > 0){ // 돌괭이 사용
+                answer += count[0]*25 + count[1]*5 + count[2];
                 picks[2]--;
-            } else {
-                break; // 더 이상 곡괭이가 없으면 종료
-            }
+            } 
+            else break; // 더 이상 곡괭이가 없으면 종료
+            
         }
 
         return answer;

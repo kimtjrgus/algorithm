e89ae0b71efed92809e318f08ee292589468b25e
Kim 김석현 <119948479+kimtjrgus@users.noreply.github.com>
2024-12-05 17:17:09 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 23.84 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
@@ -4,19 +4,20 @@ class Solution {
         // 1. 조건분기
         // 1사분면의 정점 구하기
         // 원의 특징 : 축을 제외한 1사분면의 정점 개수 *4
-        // 정점 (r1, 1)부터 (r1, (r2^2)-r1^2의 제곱근값 이하)
+        
         // x에 대해 가능한 y 좌표 범위 계산
         for (int x = 1; x <= r2; x++) {
             // 최대 반지름 r2에서 가능한 y 최대값
             long maxY = (long) Math.floor(
-                Math.sqrt((long) r2 * r2 - (long) x * x));
+                Math.sqrt((long)r2*r2 - (long)x*x));
             
             // 최소 반지름 r1에서 가능한 y 최소값
             long minY = (long) Math.ceil(
-                Math.sqrt((long) r1 * r1 - (long) x * x));
+                Math.sqrt((long)r1*r1 - (long)x*x));
             answer += (maxY - minY + 1);
         }
         
+        // 1사분면에서 구한 정점의 개수 * 4
         answer *=4;
         
         return answer;

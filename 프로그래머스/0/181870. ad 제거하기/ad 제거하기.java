78e5d374e6575467b775a1240e0a0047d0b001a0
Kim 김석현 <119948479+kimtjrgus@users.noreply.github.com>
2024-12-23 19:11:49 +0900

    [level 0] Title: ad 제거하기, Time: 5.84 ms, Memory: 94.1 MB -BaekjoonHub

--- 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -2,13 +2,8 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] strArr) {
-        List<String> resultList = new ArrayList<>();
-        
-        // 1. if(부분문자열중 ad를 포함하면) list.remove
-        for(String str : strArr){
-            if(!str.contains("ad")) resultList.add(str);
-        }
-        
-        return resultList.toArray(new String[0]);
+        return Arrays.stream(strArr)
+            .filter(s -> !s.contains("ad"))
+            .toArray(String[]::new);
     }
 }

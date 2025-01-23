76cb2020247025eab2e8b894bebdb7a600136638
Kim 김석현 <119948479+kimtjrgus@users.noreply.github.com>
2025-01-23 22:18:32 +0900

    [level 2] Title: 순위 검색, Time: 1285.21 ms, Memory: 329 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.java
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.java
@@ -1,56 +1,63 @@
 import java.util.*;
 
 class Solution {
-    static Map<String,List<Integer>> map;
-    
+    static Map<String, List<Integer>> map;
+
     public int[] solution(String[] info, String[] query) {
-        map = new HashMap<>();
-        for(String i : info){
-            String[] arr = i.split(" ");
-            comb(arr,0,"");
-        }
-
-        for(String k : map.keySet())Collections.sort(map.get(k));
-
-
         int[] answer = new int[query.length];
-        for(int i = 0; i < query.length; i++){
-            String[] q = query[i].replace(" and ","").split(" ");
-            int index = 0;
-            if(map.containsKey(q[0]))index =binary(map.get(q[0]),Integer.parseInt(q[1]));
-            answer[i] = index;
+        map = new HashMap<String, List<Integer>>(); 
+        
+        // 1. info정보를 토대로 가능한 조합 구성
+        for (int i = 0; i < info.length; i++) {
+            String[] p = info[i].split(" ");
+            makeSentence(p, "", 0);
         }
+        
+        // 2. map을 키순 정렬
+        for (String key : map.keySet())
+            Collections.sort(map.get(key));
+ 
+        // 3. 이진 탐색으로 키값 검색
+        for (int i = 0; i < query.length; i++) {
+            query[i] = query[i].replaceAll(" and ", "");
+            String[] q = query[i].split(" ");
+            answer[i] = map.containsKey(q[0]) ? binSearch(q[0], Integer.parseInt(q[1])) : 0;
+        }
+ 
         return answer;
     }
 
-    public int binary(List<Integer> list, int value){
-        int start = 0;
-        int end = list.size()-1;
-
+    // 이진 탐색
+    public int binSearch(String key, int score) {
+        List<Integer> list = map.get(key);
+        int start = 0, end = list.size() - 1;
+ 
         while (start <= end) {
             int mid = (start + end) / 2;
-            if (list.get(mid) < value)
+            if (list.get(mid) < score)
                 start = mid + 1;
             else
                 end = mid - 1;
         }
+ 
         return list.size() - start;
-    }
-
-    public void comb(String[] info, int index, String result){
-        if(index == 4){
-            if(!map.containsKey(result)){
-                List<Integer> list = new ArrayList<>();
-                map.put(result,list);
+    }  
+    
+    // info가 포함될 수 있는 문장 조합을 구함.
+    private void makeSentence(String[] p, String str, int cnt) {
+        if (cnt == 4) {
+            if (!map.containsKey(str)) {
+                List<Integer> list = new ArrayList<Integer>();
+                map.put(str, list);
             }
-            map.get(result).add(Integer.parseInt(info[4]));
+            map.get(str).add(Integer.parseInt(p[4]));
             return;
         }
-
-        comb(info,index+1, result+"-");
-        comb(info,index+1,result+info[index]);
+        makeSentence(p, str + "-", cnt + 1);
+        makeSentence(p, str + p[cnt], cnt + 1);
     }
 }
+
 
 
 /* 문제 풀이
@@ -58,10 +65,5 @@ class Solution {
 개발팀의 문의조건 1차원 배열 query
 -> 각 문의 조건에 해당하는 사람 수를 순서대로 배열에 담아 return.
 
-지원자 정보를 배열화하는 메서드
-문의 조건을 배열화하는 메서드를 각각 구해
-
-1차원 배열을 split하여 2차원배열로 저장하는 방법
-정보와 문의 조건을 매치하여 count할 것
-조건 정보 중 "-" 는 해당 조건은 고려않고 모두 포함시킨다는 의미.
+이진탐색, map
 */

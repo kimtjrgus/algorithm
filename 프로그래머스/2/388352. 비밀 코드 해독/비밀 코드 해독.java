9b5e9a6c3a9128bb690c4315b35dbe6fb9ff9479
Kim 김석현 <119948479+kimtjrgus@users.noreply.github.com>
2025-04-22 20:02:37 +0900

    [level 2] Title: 비밀 코드 해독, Time: 24.26 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
@@ -2,56 +2,57 @@ import java.util.*;
 
 class Solution {
     int answer = 0; // 비밀코드로 가능한 정수 조합 갯수
-    int[][] qGlobal;
-    int[] ansGlobal;
-    int n;
+    int[][] q;
+    int[] ans;
+    int n, m;
 
     public int solution(int n, int[][] q, int[] ans) {
         this.n = n;
-        this.qGlobal = q;
-        this.ansGlobal = ans;
-        
+        this.q = q;
+        this.ans = ans;
+        this.m = ans.length;
         
         // 1. 비밀코드로 만들 1~n 사이 정수 5개를 뽑아 조합 생성
-        // 2. 생성된 조합을 2차원배열 q와 ans배열 값 비교
+        // 2. 생성된 조합을 2차원배열 q와 ans배열 값 비교 isValid
         // 3. 2번 조건에 적합하다면 answer++
         
-        List<Integer> current = new ArrayList<>();
-        comb(1, current);
+        int[] comb = new int[5];
+        generateComb(0, 1, comb);
 
         return answer;
     }
 
     // 1~n 사이 서로 다른 정수 5개 조합을 생성
-    void comb(int start, List<Integer> current) {
-        if (current.size() == 5) {
-            if (isValid(current)) {
-                answer++;
-            }
+    void generateComb(int depth, int start, int[] comb) {
+        if (depth == 5) {
+            if (isValid(comb)) answer++;
+            
             return;
         }
 
         for (int i = start; i <= n; i++) {
-            current.add(i);
-            comb(i + 1, current);
-            current.remove(current.size() - 1); // 백트래킹
+            comb[depth] = i;
+            generateComb(depth + 1, i + 1, comb); 
         }
     }
 
     // q, ans 조건을 만족하는지 검사
-    boolean isValid(List<Integer> comb) {
-        // set을 사용해 중복을 제거
-        Set<Integer> codeSet = new HashSet<>(comb);
+    boolean isValid(int[] comb) {
+        for (int i = 0; i < m; i++) {
+            int cnt = 0;
 
-        for (int i = 0; i < qGlobal.length; i++) {
-            int count = 0;
-            for (int val : qGlobal[i]) {
-                if (codeSet.contains(val)) count++;
+            // q[i].length == 5, comb.length == 5 -> 작은 루프
+            for (int j = 0; j < 5; j++) {
+                for (int k = 0; k < 5; k++) {
+                    if (comb[k] == q[i][j]) {
+                        cnt++;
+                        break;
+                    }
+                }
             }
 
-            if (count != ansGlobal[i]) {
-                return false;
-            }
+            // 조기 탈락
+            if (cnt != ans[i]) return false;
         }
 
         return true;

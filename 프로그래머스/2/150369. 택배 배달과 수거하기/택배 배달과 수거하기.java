e38f435ac5add47745f0ceeba584487cd2ddd13f
Kim 김석현 <119948479+kimtjrgus@users.noreply.github.com>
2025-05-08 17:40:22 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 10.73 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -3,9 +3,9 @@ class Solution {
         long answer = 0;
         // greedy : 트럭 하나로 모든 배달과 수거를 마치고 
         // 물류창고까지 돌아올 수 있는 최소 이동 거리를 return 
-        
-        int d = 0;
-        int p = 0;
+        // 처음 갈때 가장 멀리 가서 배달, 수거하는 것이 가장 효과적
+        int d = 0; // 배달한 양
+        int p = 0; // 수거한 양
         for(int i=n-1; i>=0; i--){
             d -= deliveries[i];
             p -= pickups[i];
@@ -13,7 +13,7 @@ class Solution {
             while(d < 0 || p < 0){
                 d += cap;
                 p += cap;
-                answer += (i+1)*2;
+                answer += (i+1)*2; // 가고 오는 거리기 때문에 *2
             }
         }
         
